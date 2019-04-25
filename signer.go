c7d4f61e60bbcf0037d36e1632af841a54478c49
Stanislav Dagaev <460sochi@gmail.com>
2019-04-26 00:23:08 +0300

    Successfully paralleled Multihash Func Main test time reduced from 47s to 8s

--- signer.go
+++ signer.go
@@ -6,6 +6,7 @@ import (
 	"sort"
 	"strconv"
 	"sync"
+	"sync/atomic"
 )
 
 var md5Quote = 1
@@ -17,17 +18,22 @@ func main() {
 	// wg := &sync.WaitGroup{}
 	in := make(chan interface{})
 	out := make(chan interface{})
+	// in2 := make(chan interface{})
+	out2 := make(chan interface{})
 	go SingleHash(in, out)
 	in <- 1
 	close(in)
 
-	fmt.Println("DataSignerCrc32", DataSignerCrc32(strconv.Itoa(0)))
-	for r := range out {
-		fmt.Println(r)
-	}
+	// fmt.Println("DataSignerCrc32", DataSignerCrc32(strconv.Itoa(0)))
+	// for r := range out {
+	// 	fmt.Println(r)
+	// }
 
-	// res2 := MultiHash(res)
-	// fmt.Println(res2)
+	go MultiHash(out, out2)
+
+	res := <-out2
+
+	fmt.Println("res", res)
 	// res3 := CombineResult(res2)
 	// fmt.Println(res3)
 }
@@ -54,13 +60,19 @@ func chanMaker(n int) (list chSlice) {
 func ExecutePipeline(jobs ...job) {
 	var wg sync.WaitGroup
 	chans := chanMaker(len(jobs))
-	counter := 0
+	var counter uint64
 
 	for i, jb := range jobs {
 		if i != len(jobs)-1 {
 			go jb(chans[i].jobIn, chans[i].out)
 		}
 	}
+
+	// for i, jb := range jobs {
+	// 	if i == 0 || i == len(jobs)-1 || i == len(jobs)-2 {
+	// 		go jb(chans[i].jobIn, chans[i].out)
+	// 	}
+	// }
 
 	for i, jb := range jobs {
 		i := i
@@ -75,7 +87,7 @@ func ExecutePipeline(jobs ...job) {
 					select {
 					case data := <-out:
 						fmt.Println("Worker1 listener: ", data)
-						counter++
+						atomic.AddUint64(&counter, 1)
 						chans[i+1].in <- data
 					default:
 						fmt.Println("Worker1: Ended")
@@ -91,10 +103,11 @@ func ExecutePipeline(jobs ...job) {
 
 			go func(jobIn, in, out chan interface{}, wg *sync.WaitGroup) {
 				defer wg.Done()
-				cnt := 0
+				var cnt uint64
 
 				for data := range in {
 					fmt.Printf("Worker%v got data from jb%v: %v\n", i, i-1, data)
+					// go jb(chans[i].jobIn, chans[i].out)
 					jobIn <- data
 				}
 				close(jobIn)
@@ -102,7 +115,7 @@ func ExecutePipeline(jobs ...job) {
 				for data := range chans[i].out {
 					fmt.Printf("Worker%v Recieved data from Job%v: %v\n", i, i, data)
 					chans[i+1].in <- data
-					cnt++
+					atomic.AddUint64(&cnt, 1)
 
 					if cnt == counter {
 						close(chans[i+1].in)
@@ -118,12 +131,12 @@ func ExecutePipeline(jobs ...job) {
 
 			go func(jobIn, in, out chan interface{}, wg *sync.WaitGroup) {
 				defer wg.Done()
-				cnt := 0
+				var cnt uint64
 
 				for data := range in {
 					fmt.Println("Worker Last: ", data)
 					jobIn <- data
-					cnt++
+					atomic.AddUint64(&cnt, 1)
 
 					fmt.Println("counter", cnt, counter)
 
@@ -156,8 +169,9 @@ func SingleHash(in, out chan interface{}) {
 		}
 
 		ds1ch := DataSigner(strconv.Itoa(data))
+		md5 := DataSignerMd5(strconv.Itoa(data))
 
-		ds2result := DataSignerCrc32(DataSignerMd5(strconv.Itoa(data)))
+		ds2result := DataSignerCrc32(md5)
 		ds1result := <-ds1ch
 
 		result := ds1result + "~" + ds2result
@@ -165,18 +179,18 @@ func SingleHash(in, out chan interface{}) {
 		out <- result
 		// close(out)
 	}
-	// fmt.Println("SingleHash ended")
+	fmt.Println("SingleHash ended")
 	return
 }
 
 func DataSigner(data string) chan string {
-	fmt.Println("DataSigner", data)
+	// fmt.Println("DataSigner", data)
 	resultCh := make(chan string, 1)
 	go func(ch chan string) {
 		d := DataSignerCrc32(data)
 		ch <- d
 	}(resultCh)
-	fmt.Println("DataSigner ended")
+	// fmt.Println("DataSigner ended")
 	return resultCh
 }
 
@@ -186,11 +200,17 @@ func MultiHash(in, out chan interface{}) {
 		if !ok {
 			errors.New("Not a string")
 		}
-		var result string
-		for i := 0; i < 6; i++ {
-			part := DataSignerCrc32(strconv.Itoa(i) + data)
-			result += part
-		}
+		partCh1 := DataSigner(strconv.Itoa(0) + data)
+		partCh2 := DataSigner(strconv.Itoa(1) + data)
+		partCh3 := DataSigner(strconv.Itoa(2) + data)
+		partCh4 := DataSigner(strconv.Itoa(3) + data)
+		partCh5 := DataSigner(strconv.Itoa(4) + data)
+		partCh6 := DataSigner(strconv.Itoa(5) + data)
+
+		part1, part2, part3, part4, part5, part6 := <-partCh1, <-partCh2, <-partCh3, <-partCh4, <-partCh5, <-partCh6
+
+		result := part1 + part2 + part3 + part4 + part5 + part6
+		// fmt.Println("result", result)
 		out <- result
 	}
 }
